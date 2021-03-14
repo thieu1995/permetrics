6d1102fe7ae641f3d2909b226c1d01d5bc063a4b
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-03-15 05:42:03 +0700

    Testing new version

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ def readme():
 
 setup(
     name="permetrics",
-    version="1.1.0",
+    version="1.1.0-alpha",
     author="Thieu Nguyen",
     author_email="nguyenthieu2102@gmail.com",
     description="PerMetrics: A framework of PERformance METRICS for machine learning models",
     long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/thieu1995/permetrics",
-    download_url="https://github.com/thieu1995/permetrics/archive/v1.1.0.zip",
+    download_url="https://github.com/thieu1995/permetrics/archive/v1.1.1-alpha.zip",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
