650b7f58519486528744a40e7196cca3375ab016
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-03-24 23:59:35 +0700

    Update CE

--- permetrics/regression.py
+++ permetrics/regression.py
@@ -784,10 +784,11 @@ class RegressionMetric:
 
     def entropy(self, y_true=None, y_pred=None, multi_output="raw_values", decimal=None, clean=True, positive_only=True):
         """
-        Entropy Loss (E): Best possible score is , bigger value is . Range =
+        Entropy Loss (E): Best possible score is 0.0, smaller value is better . Range = [0, +inf)
 
         Notes
         ~~~~~
+            + Greater value of Entropy, the greater the uncertainty for probability distribution and smaller the value the less the uncertainty
             + https://datascience.stackexchange.com/questions/20296/cross-entropy-loss-explanation
 
         Args:
@@ -809,25 +810,44 @@ class RegressionMetric:
             result = -np.sum(y_true * np.log(y_pred.clip(self.EPSILON, None)), axis=0)
             return self.__multi_output_result(result, multi_output, decimal)
 
-    def cross_entropy(self, clean=False, multi_output="raw_values", decimal=3, **kwargs):
-        y_true, y_pred, onedim = self.get_clean_data(clean, kwargs)
-        if onedim:
-            f_true, intervals = histogram(y_true, bins=len(unique(y_true)) - 1)
-            intervals[0] = min([min(y_true), min(y_pred)])
-            intervals[-1] = max([max(y_true), max(y_pred)])
+    def cross_entropy(self, y_true=None, y_pred=None, multi_output="raw_values", decimal=None, clean=True, positive_only=True):
+        """
+        Cross Entropy (CE): Best possible score is 0.0, smaller value is better . Range = [0, 1]
+
+        Notes
+        ~~~~~
+            + https://datascience.stackexchange.com/questions/20296/cross-entropy-loss-explanation
+
+        Args:
+            y_true (tuple, list, np.ndarray): The ground truth values
+            y_pred (tuple, list, np.ndarray): The prediction values
+            multi_output: Can be "raw_values" or list weights of variables such as [0.5, 0.2, 0.3] for 3 columns, (Optional, default = "raw_values")
+            decimal (int): The number of fractional parts after the decimal point (Optional, default = 5)
+            clean (bool): Remove all rows contain 0 value in y_pred (some methods have denominator is y_pred) (Optional, default = True)
+            positive_only (bool): Calculate metric based on positive values only or not (Optional, default = True)
+
+        Returns:
+            result (float, int, np.ndarray): CE metric for single column or multiple columns
+        """
+        y_true, y_pred, one_dim, decimal = self.get_preprocessed_data(y_true, y_pred, clean, decimal, positive_only)
+
+        if one_dim:
+            f_true, intervals = np.histogram(y_true, bins=len(np.unique(y_true)) - 1)
+            intervals[0] = np.min([np.min(y_true), np.min(y_pred)])
+            intervals[-1] = np.max([np.max(y_true), np.max(y_pred)])
             f_true = f_true / len(f_true)
-            f_pred = histogram(y_pred, bins=intervals)[0] / len(y_pred)
-            value = self.entropy(clean, None, decimal, y_true=f_true, y_pred=f_pred)
-            return round(value, decimal)
+            f_pred = np.histogram(y_pred, bins=intervals)[0] / len(y_pred)
+            score = self.entropy(f_true, f_pred, multi_output, decimal, clean=True, positive_only=True)
+            return np.round(score, decimal)
         else:
             score = []
             for i in range(y_true.shape[1]):
-                f_true, intervals = histogram(y_true[:,i], bins=len(unique(y_true[:,i])) - 1)
-                intervals[0] = min([min(y_true[:,i]), min(y_pred[:,i])])
-                intervals[-1] = max([max(y_true[:,i]), max(y_pred[:,i])])
+                f_true, intervals = np.histogram(y_true[:, i], bins=len(np.unique(y_true[:, i])) - 1)
+                intervals[0] = np.min([np.min(y_true[:, i]), np.min(y_pred[:, i])])
+                intervals[-1] = np.max([np.max(y_true[:, i]), np.max(y_pred[:, i])])
                 f_true = f_true / len(f_true)
-                f_pred = histogram(y_pred[:,i], bins=intervals)[0] / len(y_pred[:,i])
-                score.append(self.entropy(clean, "raw_values", decimal, y_true=f_true, y_pred=f_pred))
+                f_pred = np.histogram(y_pred[:, i], bins=intervals)[0] / len(y_pred[:, i])
+                score.append(self.entropy(f_true, f_pred, multi_output, decimal, clean=True, positive_only=True))
             return self.__multi_output_result(score, multi_output, decimal)
 
     def kullback_leibler_divergence(self, clean=False, multi_output="raw_values", decimal=3, **kwargs):
