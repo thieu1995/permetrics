ee641ae456ecd4dcc32d7c6e4a39f996f5fdaec3
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-03-25 19:03:36 +0700

    Update setup file

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/thieu1995/permetrics",
     download_url="https://github.com/thieu1995/permetrics/archive/v1.2.0.zip",
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*', 'examples*']),
     include_package_data=True,
     license="MIT",
     classifiers=[
