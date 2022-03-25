8790d2d8da7d9fb110e6bc6a2eb4a93a35b64849
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-03-25 14:38:52 +0700

    Update permetrics

--- permetrics/__init__.py
+++ permetrics/__init__.py
@@ -3,6 +3,27 @@
 #       Email: nguyenthieu2102@gmail.com            %
 #       Github: https://github.com/thieu1995        %
 # --------------------------------------------------%
+#
+# ## 1. Import packages, classes
+# ## 2. Create object
+# ## 3. From object calls function and use
+#
+# import numpy as np
+# from permetrics.regression import RegressionMetric
+#
+# y_true = np.array([3, -0.5, 2, 7, 5, 6])
+# y_pred = np.array([2.5, 0.0, 2, 8, 5, 6])
+#
+# evaluator = RegressionMetric(y_true, y_pred, decimal=5)
+#
+# ## Get the result of any function you want to
+#
+# rmse = evaluator.RMSE()
+# mse = evaluator.MSE()
+# mae = evaluator.MAE()
+#
+# print(f"RMSE: {rmse}, MSE: {mse}, MAE: {mae}")
+
 
 __version__ = "1.2.0"
 
