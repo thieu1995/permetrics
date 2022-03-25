d7beaf34cb5b500d466272c30bd0b3ed10412e86
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-03-25 11:11:36 +0700

    Update ClassificationMetric

--- permetrics/classification.py
+++ permetrics/classification.py
@@ -1,91 +1,81 @@
-#!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu Nguyen" at 09:29, 23/09/2020                                                        %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
-#       Github:     https://github.com/thieu1995                                                        %
-# -------------------------------------------------------------------------------------------------------%
+# !/usr/bin/env python
+# Created by "Thieu" at 09:29, 23/09/2020 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
+
+import numpy as np
+from permetrics.evaluator import Evaluator
 
 
-from numpy import max, round, sqrt, abs, mean, dot, divide, arctan, sum, any, median, log, var, std
-from numpy import ndarray, array, isfinite, isnan, argsort, zeros, concatenate
-
-
-class Metrics:
+class ClassificationMetric(Evaluator):
     """
-        https://scikit-learn.org/stable/modules/model_evaluation.html#regression-metrics
+    This is class contains all classification metrics (for both binary and multiple classification problem)
+
+    Notes
+    ~~~~~
+    + Extension of: https://scikit-learn.org/stable/modules/model_evaluation.html#classification-metrics
     """
 
-    def __init__(self, y_true, y_pred):
+    def __init__(self, y_true=None, y_pred=None, decimal=5, **kwargs):
         """
-        :param y_true:
-        :param y_pred:
+        Args:
+            y_true (tuple, list, np.ndarray): The ground truth values
+            y_pred (tuple, list, np.ndarray): The prediction values
+            decimal (int): The number of fractional parts after the decimal point
+            **kwargs ():
         """
-        self.onedim = False
-        if type(y_true) is ndarray and type(y_pred) is ndarray:
-            y_true = y_true.astype('float64')
-            y_pred = y_pred.astype('float64')  # x = x[~np.isnan(x)] can't remove if array is dtype object, only work with dtype float
-            if y_true.ndim == 1 and y_pred.ndim == 1:
-                self.onedim = True
-                ## Remove all Nan in y_pred
-                y_true = y_true[~isnan(y_pred)]
-                y_pred = y_pred[~isnan(y_pred)]
-                ## Remove all Inf in y_pred
-                self.y_true = y_true[isfinite(y_pred)]
-                self.y_pred = y_pred[isfinite(y_pred)]
+        super().__init__(y_true, y_pred, decimal, **kwargs)
+        if kwargs is None: kwargs = {}
+        self.set_keyword_arguments(kwargs)
 
-                self.y_true_clean = self.y_true[self.y_pred != 0]
-                self.y_pred_clean = self.y_pred[self.y_pred != 0]
-            else:
-                if y_true.ndim == y_pred.ndim:
-                    ## Remove all row with Nan in y_pred
-                    y_true = y_true[~isnan(y_pred).any(axis=1)]
-                    y_pred = y_pred[~isnan(y_pred).any(axis=1)]
-                    ## Remove all row with Inf in y_pred
-                    self.y_true = y_true[isfinite(y_pred).any(axis=1)]
-                    self.y_pred = y_pred[isfinite(y_pred).any(axis=1)]
+    def mean_log_likelihood(self, y_true=None, y_pred=None, multi_output="raw_values", decimal=None, clean=True, positive_only=True):
+        """
+        Mean Log Likelihood (MLL): Best possible score is ..., the higher value is better. Range = (-inf, +inf)
+        Link:
+            + https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/elementwise.py#L235
 
-                    self.y_true_clean = self.y_true[~any(self.y_pred == 0, axis=1)]
-                    self.y_pred_clean = self.y_pred[~any(self.y_pred == 0, axis=1)]
-                else:
-                    print("=====Failed! y_true and y_pred need to have same number of dimensions=======")
-                    exit(0)
+        Args:
+            y_true (tuple, list, np.ndarray): The ground truth values
+            y_pred (tuple, list, np.ndarray): The prediction values
+            multi_output: Can be "raw_values" or list weights of variables such as [0.5, 0.2, 0.3] for 3 columns, (Optional, default = "raw_values")
+            decimal (int): The number of fractional parts after the decimal point (Optional, default = 5)
+            clean (bool): Remove all rows contain 0 value in y_pred (some methods have denominator is y_pred) (Optional, default = True)
+            positive_only (bool): Calculate metric based on positive values only or not (Optional, default = True)
+
+        Returns:
+            result (float, int, np.ndarray): MLL metric
+        """
+        y_true, y_pred, one_dim, decimal = self.get_preprocessed_data(y_true, y_pred, clean, decimal, positive_only)
+        if one_dim:
+            return np.round(np.mean(-(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))), decimal)
         else:
-            print("=====Failed! y_true and y_pred need to be a ndarray=======")
-            exit(0)
+            result = np.mean(-(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)), axis=0)
+            return self.__multi_output_result(result, multi_output, decimal)
 
-    def __multi_output_result__(self, result=None, multi_output=None, decimal=None):
-        if multi_output is None:
-            return round(mean(result), decimal)
-        elif isinstance(multi_output, (tuple, list, set)):
-            weights = array(multi_output)
-            if self.y_true.ndim != len(weights):
-                print("==========Failed! Multi-output weights has different length with y_true==============")
-                exit(0)
-            return round(dot(result, multi_output), decimal)
-        elif isinstance(multi_output, ndarray) and len(multi_output) == self.y_true.ndim:
-            return round(dot(result, multi_output), decimal)
-        elif multi_output == "raw_values":
-            return round(result, decimal)
-        else:
-            print("==========Failed! Multi-output not supported==============")
-            exit(0)
-
-    def mll_func(self, clean=False, multi_output="raw_values", decimal=3):
+    def single_log_likelihood(self, y_true=None, y_pred=None, decimal=None, clean=True, positive_only=True):
         """
-            Mean Log Likelihood (MLL)
-            https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/elementwise.py#L235
+        Log Likelihood (LL): Best possible score is ..., the higher value is better. Range = (-inf, +inf)
+
+        Notes
+        ~~~~~
+            + Computes the log likelihood between two numbers, or for element between a pair of list, tuple or numpy arrays.
+
+        Args:
+            y_true (tuple, list, np.ndarray): The ground truth values
+            y_pred (tuple, list, np.ndarray): The prediction values
+            multi_output: Can be "raw_values" or list weights of variables such as [0.5, 0.2, 0.3] for 3 columns, (Optional, default = "raw_values")
+            decimal (int): The number of fractional parts after the decimal point (Optional, default = 5)
+            clean (bool): Remove all rows contain 0 value in y_pred (some methods have denominator is y_pred) (Optional, default = True)
+            positive_only (bool): Calculate metric based on positive values only or not (Optional, default = True)
+
+        Returns:
+            result (float, int, np.ndarray): LL metric
         """
-        y_true, y_pred = self.y_true, self.y_pred
-        if clean:
-            y_true, y_pred = self.y_true_clean, self.y_pred_clean
+        y_true, y_pred, one_dim, decimal = self.get_preprocessed_data(y_true, y_pred, clean, decimal, positive_only)
+        return np.round(-(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)), decimal)
 
-        if self.onedim:
-            score = -(y_true * log(y_pred) + (1 - y_true) * log(1 - y_pred))
-            return round(mean(score), decimal)
-        else:
-            score = -(y_true * log(y_pred) + (1 - y_true) * log(1 - y_pred))
-            return self.__multi_output_result__(mean(score, axis=0), multi_output, decimal)
 
-    MLL = mll = mll_func
+    MLL = mll = mean_log_likelihood
+
+    LL = ll = single_log_likelihood
