8723a5ab0c8180748d0d8c2071c3b7e43cfa1828
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-07-26 18:12:49 +0700

    Update ClusteringMemtric

--- permetrics/clustering.py
+++ permetrics/clustering.py
@@ -13,9 +13,11 @@ from itertools import chain
 import numpy as np
 from permetrics.evaluator import Evaluator
 from permetrics.utils.data_util import *
-from permetrics.utils.cluster_util import (get_min_dist, get_centroids, general_sums_of_squares, pdist,
-                                cdist, squareform, get_labels,
-                                average_scattering, cluster_sep, density_between, density_clusters, pmatch)
+from permetrics.utils import cluster_util as cu
+# from permetrics.utils.cluster_util import (get_min_dist, get_centroids, general_sums_of_squares, pdist,
+#                                 cdist, squareform, get_labels,
+#                                 average_scattering, cluster_sep, density_between, density_clusters, pmatch)
+from permetrics.utils.encoder import LabelEncoder
 
 
 class ClusteringMetric(Evaluator):
@@ -41,42 +43,59 @@ class ClusteringMetric(Evaluator):
         if kwargs is None: kwargs = {}
         self.set_keyword_arguments(kwargs)
         self.X = X
-        self.binary = True
-        self.representor = "number"  # "number" or "string"
-        self.le = None  # LabelEncoder
+        self.le = None
 
-    def get_processed_data(self, y_true=None, y_pred=None, decimal=None):
+    def get_processed_external_data(self, y_true=None, y_pred=None, decimal=None):
         """
         Args:
             y_true (tuple, list, np.ndarray): The ground truth values
             y_pred (tuple, list, np.ndarray): The prediction values
-            clean (bool): Remove all rows contain 0 value in y_pred (some methods have denominator is y_pred)
             decimal (int, None): The number of fractional parts after the decimal point
 
         Returns:
             y_true_final: y_true used in evaluation process.
             y_pred_final: y_pred used in evaluation process
-            one_dim: is y_true has 1 dimensions or not
+            le: label encoder object
             decimal: The number of fractional parts after the decimal point
         """
         decimal = self.decimal if decimal is None else decimal
-        if (y_true is not None) and (y_pred is not None):
-            y_true, y_pred, binary, representor = format_classification_data(y_true, y_pred)
-        else:
-            if (self.y_true is not None) and (self.y_pred is not None):
-                y_true, y_pred, binary, representor = format_classification_data(self.y_true, self.y_pred)
+        if y_pred is None:              # Check for function called
+            if self.y_pred is None:     # Check for object of class called
+                raise ValueError("You need to pass y_true and y_pred to calculate external clustering metrics.")
             else:
-                raise ValueError("y_true or y_pred is None. You need to pass y_true and y_pred to object creation or function called.")
-        return y_true, y_pred, binary, representor, decimal
+                if self.y_true is None:
+                    # y_true, y_pred, self.le = format_internal_clustering_data(self.y_pred)
+                    raise ValueError("You need to pass y_true and y_pred to calculate external clustering metrics.")
+                else:
+                    y_true, y_pred, self.le = format_external_clustering_data(self.y_true, self.y_pred)
+        else:   # This is for function called, it will override object of class called
+            if y_true is None:
+                # y_true, y_pred, self.le = format_internal_clustering_data(y_pred)
+                raise ValueError("You need to pass y_true and y_pred to calculate external clustering metrics.")
+            else:
+                y_true, y_pred, self.le = format_external_clustering_data(y_true, y_pred)
+        return y_true, y_pred, self.le, decimal
 
-    def get_processed_y_pred(self, y_pred=None):
-        if y_pred is not None:
-            return format_clustering_label(y_pred)
-        else:
-            if self.y_pred is None:
-                raise ValueError("To calculate clustering metrics, you need to pass y_pred")
+    def get_processed_internal_data(self, y_pred=None, decimal=None):
+        """
+        Args:
+            y_pred (tuple, list, np.ndarray): The prediction values
+            decimal (int, None): The number of fractional parts after the decimal point
+
+        Returns:
+            y_pred_final: y_pred used in evaluation process
+            le: label encoder object
+            decimal: The number of fractional parts after the decimal point
+        """
+        decimal = self.decimal if decimal is None else decimal
+        if y_pred is None:              # Check for function called
+            if self.y_pred is None:     # Check for object of class called
+                raise ValueError("You need to pass y_pred to calculate external clustering metrics.")
             else:
-                return format_clustering_label(self.y_pred)
+                y_pred, self.le = format_internal_clustering_data(self.y_pred)
+        else:   # This is for function called, it will override object of class called
+            y_pred, self.le = format_internal_clustering_data(y_pred)
+        return y_pred, self.le, decimal
 
     def check_X(self, X):
         if X is None:
@@ -100,21 +119,26 @@ class ClusteringMetric(Evaluator):
             BH (float): The Ball-Hall index
         """
         X = self.check_X(X)
-        y_pred = self.get_processed_y_pred(y_pred)
-        n_classes = len(np.unique(y_pred))
-        wgss = []
+        y_pred, _, decimal = self.get_processed_internal_data(y_pred)
+        n_clusters = len(np.unique(y_pred))
+        wgss = 0
         ## For each cluster, find the centroid and then the within-group SSE
-        for k in range(n_classes):
+        for k in range(n_clusters):
             centroid_mask = y_pred == k
             cluster_k = X[centroid_mask]
             centroid = np.mean(cluster_k, axis=0)
-            wgss.append(np.sum((cluster_k - centroid) ** 2))
-        return np.sum(wgss) / n_classes
+            wgss += np.sum((cluster_k - centroid) ** 2)
+        return np.round(wgss / n_clusters, decimal)
 
     def calinski_harabasz_score(self, X=None, y_pred=None, **kwargs):
         """
         Compute the Calinski and Harabasz (1974) index. It is also known as the Variance Ratio Criterion.
         The score is defined as ratio between the within-cluster dispersion and the between-cluster dispersion.
+
+        Notes:
+        ~~~~~~
+            + This metric in scikit-learn library is wrong in calculate the intra_disp variable (WGSS)
+            + https://github.com/scikit-learn/scikit-learn/blob/7f9bad99d/sklearn/metrics/cluster/_unsupervised.py#L351C1-L351C1
 
         Args:
             X (array-like of shape (n_samples, n_features)):
@@ -129,12 +153,12 @@ class ClusteringMetric(Evaluator):
             analysis". Communications in Statistics <https://www.tandfonline.com/doi/abs/10.1080/03610927408827101>`_
         """
         X = self.check_X(X)
-        y_pred = self.get_processed_y_pred(y_pred)
+        y_pred, _, decimal = self.get_processed_internal_data(y_pred)
         n_samples, n_vars = X.shape
-        n_classes = len(np.unique(y_pred))
-        denom= (general_sums_of_squares(X, y_pred)["WGSS"] * (n_classes - 1))
-        numer = (general_sums_of_squares(X, y_pred)["BGSS"] * (n_samples - n_classes))
-        return numer / denom
+        n_clusters = len(np.unique(y_pred))
+        numer = cu.compute_BGSS(X, y_pred) * (n_samples - n_clusters)
+        denom = cu.compute_WGSS(X, y_pred) * (n_clusters - 1)
+        return np.round(numer / denom, decimal)
 
     def xie_beni_index(self, X=None, y_pred=None, **kwargs):
         """
@@ -154,17 +178,13 @@ class ClusteringMetric(Evaluator):
             xb (float): The Xie-Beni index
         """
         X = self.check_X(X)
-        y_pred = self.get_processed_y_pred(y_pred)
+        y_pred, _, decimal = self.get_processed_internal_data(y_pred)
         # Get the centroids
-        centroids = get_centroids(X, y_pred)
-        euc_distance_to_centroids = get_min_dist(X, centroids)
+        centroids = cu.get_centroids(X, y_pred)
+        euc_distance_to_centroids = cu.get_min_dist(X, centroids)
         WGSS = np.sum(euc_distance_to_centroids**2)
         # Computing the minimum squared distance to the centroids:
-        MinSqDist = np.min(pdist(centroids, metric='sqeuclidean'))
+        MinSqDist = np.min(cu.pdist(centroids, metric='sqeuclidean'))
         # Computing the XB index:
         xb = (1 / X.shape[0]) * (WGSS / MinSqDist)
         return xb
-
-    BHI = ball_hall_index
-    CHS = calinski_harabasz_score
-    XBI = xie_beni_index
