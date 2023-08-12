1d8fc961eb1601c90cc2b4d4ff12e256f2dbc38f
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-08-12 11:51:51 +0700

    Add constant.py

---
+++ permetrics/utils/constant.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 23:22, 11/08/2023 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+
+
+SUPPORTED_LIST = (list, tuple, np.ndarray)
