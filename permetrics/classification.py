b0d178fc2f07bf9a78167c74bed65987d924bf6c
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-08-25 09:55:59 +0700

    Update comment

--- permetrics/classification.py
+++ permetrics/classification.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
 # Created by "Thieu" at 09:29, 23/09/2020 ----------%
 #       Email: nguyenthieu2102@gmail.com            %
 #       Github: https://github.com/thieu1995        %
